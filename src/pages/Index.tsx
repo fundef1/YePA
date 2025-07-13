86a93a008b29e74a01759a204f0dc9bec630bbbc
[dyad] <git@dyad.sh>
2025-07-13 18:17:25 +0200

    [dyad] Fixed component naming and export consistency - wrote 2 file(s), renamed 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,10 @@
 import { MadeWithDyad } from "@/components/made-with-dyad";
-import EpubProcessor from "@/components/EpubProcessor";
+import ZipProcessor from "@/components/ZipProcessor";
 
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
-      <EpubProcessor />
+      <ZipProcessor />
     </div>
   );
 };
