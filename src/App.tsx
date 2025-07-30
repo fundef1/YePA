89154b9ad00ec161966c22b1d896be0637449874
[dyad] <undefided@free.fr>
2025-07-30 18:20:28 +0200

    [dyad] Fixed GitHub Pages routing by setting basename in React Router and configuring deploy workflow to copy index.html to 404.html and add .nojekyll. - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Index from './pages/Index'; // Assuming this is your main page
+import { FooterAttribution } from './components/FooterAttribution'; // Assuming this is used
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <Router basename="/YePA"> {/* Set the basename to your repository name */}
+      <div className="min-h-screen flex flex-col">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* Add other routes here if you have them */}
+          </Routes>
+        </main>
+        <FooterAttribution />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
