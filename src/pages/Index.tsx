9703d50d566f9deab29c8948afe05ea6f34d5bad
[dyad] <undefided@free.fr>
2025-08-03 17:08:14 +0200

    [dyad] Fix button width jitter - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -203,7 +203,7 @@ export default function Index() {
                                 className="absolute top-0 left-0 h-full bg-primary/50 dark:bg-primary/30"
                                 style={{ width: `${progress}%`, transition: 'width 0.1s ease-in-out' }}
                               />
-                              <span className="relative z-10">
+                              <span className="relative z-10 tabular-nums">
                                 Processing... {Math.round(progress)}%
                               </span>
                             </>
